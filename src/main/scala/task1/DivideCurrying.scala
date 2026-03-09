1128b07e14ae15d2b4dcced9c92ed6bb490b6e8a
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-09 11:01:24 +0100

    fix: change den and num

--- src/main/scala/task1/DivideCurrying.scala
+++ src/main/scala/task1/DivideCurrying.scala
@@ -7,9 +7,9 @@ def divide(den: Double)(num: Double): Double =
 @main def test(): Unit =
   val x: Double = 4.0
   // Divide by 0
-  println(divide(x)(0.0))
+  println(divide(0.0)(x))
   // Divide by -0
-  println(divide(x)(-0.0))
+  println(divide(-0.0)(x))
   // Partially application
   val divideByTwo: Double => Double = divide(2.0)
   println(divideByTwo(x))
