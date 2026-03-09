8108bdf15298e1808487582be845b8156d868371
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-09 16:54:34 +0100

    feat: added Mul expression

--- src/main/scala/task4/Expr.scala
+++ src/main/scala/task4/Expr.scala
@@ -3,8 +3,10 @@ package task4
 enum Expr:
   case Literal(n: Int)
   case Add(e1: Expr, e2: Expr)
+  case Multiply(e1: Expr, e2: Expr)
 
 object Expr:
   def evaluate(expression: Expr): Int = expression match
     case Literal(n) => n
     case Add(e1, e2) => evaluate(e1) + evaluate(e2)
+    case Multiply(e1, e2) => evaluate(e1) * evaluate(e2)
--- src/test/scala/task4/ExprTest.scala
+++ src/test/scala/task4/ExprTest.scala
@@ -14,3 +14,9 @@ class ExprTest:
     val e2: Expr = Literal(2)
     val sum: Expr = Add(e1, e2)
     assertEquals(3, evaluate(sum))
+
+  @Test def canDefineAMul(): Unit =
+    val e1: Expr = Literal(1)
+    val e2: Expr = Literal(2)
+    val mul: Expr = Multiply(e1, e2)
+    assertEquals(2, evaluate(mul))
