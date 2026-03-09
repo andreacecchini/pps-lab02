baf485bb238938245a194bc9392cec85e5093790
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-09 11:35:36 +0100

    feat: part 4

--- src/main/scala/task2/Function.scala
+++ src/main/scala/task2/Function.scala
@@ -1,8 +1,27 @@
 package task2
 
 object Function:
+  /** Part 3 */
+
   def positive(x: Int): String = x match
     case x if x >= 0 => "positive"
     case _ => "negative"
 
   def neg(predicate: String => Boolean): String => Boolean = !predicate(_)
+
+  /** Part 4. */
+  // [TODO] check _ scoping
+  val p1: Int => Int => Int => Boolean = x => y => z => x <= y && y == z
+  val p2: (Int, Int, Int) => Boolean = (x, y, z) => x <= y && y == z
+  def p3(x: Int)(y: Int)(z: Int): Boolean = x <= y && y == z
+  def p4(x: Int, y: Int, z: Int): Boolean = x <= y && y == z
+
+@main def test(): Unit =
+  import Function.*
+  val x = 1
+  val y = 2
+  val z = y
+  println(p1(x)(y)(z))
+  println(p2(x, y, z))
+  println(p3(x)(y)(z))
+  println(p4(x, y, z))
