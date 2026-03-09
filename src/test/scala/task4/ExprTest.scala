68a0adfd83664c44bd1aa1c6eb3f4273007fd523
Andrea Cecchini <andrea.cecchini13@studio.unibo.it>
2026-03-09 16:53:21 +0100

    feat: added Add expression

--- src/test/scala/task4/ExprTest.scala
+++ src/test/scala/task4/ExprTest.scala
@@ -8,3 +8,9 @@ class ExprTest:
   @Test def canDefineALiteral(): Unit =
     val literal: Expr = Literal(1)
     assertEquals(1, evaluate(literal))
+
+  @Test def canDefineASum(): Unit =
+    val e1: Expr = Literal(1)
+    val e2: Expr = Literal(2)
+    val sum: Expr = Add(e1, e2)
+    assertEquals(3, evaluate(sum))
